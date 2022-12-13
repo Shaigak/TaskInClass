5ac3650cb0750beadf7033cbded698dfbcfd9d94
[shaigak] <shaigak@code.edu.az>
2022-12-13 23:41:56 +0300

    task

---
+++ ClassTest/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+using ClassTest;
+#region Class Methods
+//Student studen1=new Student();
+//studen1.name = "Shaiq";
+//studen1.age = 26;
+//studen1.address = "Sedmoy";
+////Console.WriteLine($"{studen1.name} {studen1.age} {studen1.address}");
+
+//Console.WriteLine(studen1.GetFullName());
+
+//string PhoneBaseNum = "516006566";
+
+//Student studen2=new Student();
+//studen2.name = "Cavid";
+//studen2.age = 22;
+//studen2.address = "Sulutepe";
+//studen2.phone = "+994";
+///*C*//*onsole.WriteLine($"{studen2.name} {studen2.age} {studen2.address} {studen2.phone}{PhoneBaseNum}");*/
+
+////Console.WriteLine(studen2.GetFullName());
+
+//Console.WriteLine(studen1.GetFulldata(555));
+
+
+//Book book1=new Book();
+
+//book1.name = "Xosrov";
+//book1.author = "Nizami";
+//Console.WriteLine($"{book1.name} {} );
+
+////Konstruktor Kont = new Konstruktor("text");
+#endregion
+
+
+#region Task
+/string name = "Pervin";
+//name = "Ferid";
+
+
+//StringBuilder stringBuilder = new StringBuilder("Pervin");
+
+//stringBuilder.Append("Per");
+//stringBuilder.Append("vin");
+//int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+//int[] arr2 = new int[10];
+//int[] arr3 = new int[5] { 1, 2, 3, 4, 5 };
+
+//Console.WriteLine(arr1[0] );
+//Console.WriteLine(arr1.Length);
+//Console.WriteLine(arr1.FirstOrDefault());
+
+
+//Array.Resize{ref arr1,10 };
+
+//arr1[8] = 100;
+
+//Console.WriteLine(arr1[8]);
+
+//Console.WriteLine(arr1.Length);
+
+
+//var newArr = arr1.Clone();
+
+//int[] arr4 = { 1, 2, 3 };
+//arr1.CopyTo(arr4, 2);
+
+
+//Array.Reverse(arr1);
+
+
+//foreach (var item in arr1)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+//Console.WriteLine(array2D[0,1]);
+
+
+//string name = "Chinare";
+
+//var test = name.Reverse().ToArray();
+
+//string res = " ";
+
+//foreach (var item in test)
+//{
+//    res += item;
+
+//}
+
+//Console.WriteLine(res
+//
+
+
+//string name = "Chinare";
+//static string  ReverseStr(string str)
+//{
+//    var reversedArray = str.Reverse().ToArray();
+
+//    string res = " ";
+//    foreach (var item in  reversedArray)
+//    {
+//        res += item;
+//    }
+//    return res;
+
+//}
+
+//var res = ReverseStr("Chinare");
+
+//Console.WriteLine(res);
+
+
+
+
+//static void  ConsoleWriteLine(string text)
+//{
+//    Console.WriteLine(text);
+//}
+
+
+//ConsoleWriteLine("salam Roya");
+//ConsoleWriteLine("sagol Konul");
+//ConsoleWriteLine("Hello Chinare");
+
+//Test(new int[] { 1,100, 2, 3, 4, 5 });
+
+//var testArr == new int[] { 1, 100, 3, 4, 5 };
+//foreach (var item in testArr)
+//{
+// Console.WriteLine(item);
+//}
+
+
+
+
+
+//static void Test(int[] array)
+//{
+// foreach (var item in array)
+// {
+// if (item == 100)
+// {
+// return;
+// }
+// }
+
+// Console.WriteLine("salam");
+//}
+#endregion
---
+++ ClassTest/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region Class
+//namespace ClassTest
+//{
+//    internal class Student
+//    {
+//        public string name;
+//        public string surname;
+//        public int age;
+//        public string address;
+//        public string phone;
+
+//        public string GetFullName()
+//        {
+//            return name + " " + surname + " " + age;
+//        }
+
+//        public string GetFulldata(int code=4444)
+//        {
+//            return $"{name} {surname} {age} {code}";
+//        }
+//    }
+//}
+
+#endregion
